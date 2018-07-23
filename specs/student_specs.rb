c6cc3723c31143bbf5bbaca69b644b9b44fcf654
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-23 15:44:06 +0100

    adds two setters

--- specs/student_specs.rb
+++ specs/student_specs.rb
@@ -6,17 +6,26 @@ require_relative("../student.rb")
 
 class TestStudent < MiniTest::Test
 
-def setup()
-  @student = Student.new("Kirsty", "G7")
-end
+  def setup()
+    @student = Student.new("Kirsty", "G7")
+  end
 
 
-def test_get_name()
-  assert_equal("Kirsty", @student.get_name())
-end
+  def test_get_name()
+    assert_equal("Kirsty", @student.get_name())
+  end
 
-def test_get_cohort()
-  assert_equal("G7", @student.get_cohort)
-end
+  def test_get_cohort()
+    assert_equal("G7", @student.get_cohort)
+  end
+  
+  def test_change_name()
+    @student.set_name("Kathleen")
+    assert_equal("Kathleen", @student.get_name)
+  end
 
+  def test_change_cohort()
+    @student.set_cohort("G8")
+    assert_equal("G8", @student.get_cohort)
+  end
 end
--- student.rb
+++ student.rb
@@ -1,16 +1,23 @@
 class Student
 
   def initialize(name, cohort)
-  @name = name
-  @cohort = cohort
+    @name = name
+    @cohort = cohort
   end
 
   def get_name
-  return @name
+    return @name
   end
 
   def get_cohort
-  return @cohort
+    return @cohort
   end
 
+  def set_name(name)
+    @name = name
+  end
+
+  def set_cohort(cohort)
+    @cohort = cohort
+  end
 end
