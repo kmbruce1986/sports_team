e09052c584a09c7a28edc1eae4aefbabe13f2649
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-23 15:55:49 +0100

    adds phrase and language methods

--- specs/student_specs.rb
+++ specs/student_specs.rb
@@ -7,7 +7,7 @@ require_relative("../student.rb")
 class TestStudent < MiniTest::Test
 
   def setup()
-    @student = Student.new("Kirsty", "G7")
+    @student = Student.new("Kirsty", "G7", "I can talk", "Ruby")
   end
 
 
@@ -18,7 +18,7 @@ class TestStudent < MiniTest::Test
   def test_get_cohort()
     assert_equal("G7", @student.get_cohort)
   end
-  
+
   def test_change_name()
     @student.set_name("Kathleen")
     assert_equal("Kathleen", @student.get_name)
@@ -27,5 +27,13 @@ class TestStudent < MiniTest::Test
   def test_change_cohort()
     @student.set_cohort("G8")
     assert_equal("G8", @student.get_cohort)
+  end
+
+  def test_student_talks()
+    assert_equal("I can talk", @student.talks)
+  end
+
+  def test_favourite_language()
+    assert_equal("I love Ruby", @student.language)
   end
 end
--- student.rb
+++ student.rb
@@ -1,8 +1,10 @@
 class Student
 
-  def initialize(name, cohort)
+  def initialize(name, cohort, phrase, favourite_language)
     @name = name
     @cohort = cohort
+    @phrase = phrase
+    @language = favourite_language
   end
 
   def get_name
@@ -19,5 +21,13 @@ class Student
 
   def set_cohort(cohort)
     @cohort = cohort
+  end
+
+  def talks
+    return @phrase
+  end
+
+  def language
+    return "I love " + @language
   end
 end
